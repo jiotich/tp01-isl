55cc99348d4e5869fefe365ed161611be8e7b0ba
jiotich <jiotich@gmail.com>
2021-03-19 17:43:51 -0300

    Update sevensegs.v

--- sevensegs.v
+++ sevensegs.v
@@ -3,6 +3,7 @@ module dispcomp;
     integer sai;
 	wire [6:0] segs;
     initial begin
+	//os Cs correspondem ao estado dos computadores, posteriormente esse input sera dado através de arquivo de texto
         c1=0;
         c2=0;
         c3=1;
@@ -14,7 +15,7 @@ module dispcomp;
 		
         if (c1 & c2 & c3 & c4 & c5)
             $display ("1111110");
-        else if ((c1 & c2 & c3 & c4) | (c1 & c2 & c3 & c5) | (c1 & c2 & c4 & c5) | (c1 & c2 & c3 & c4) | (c2 & c3 & c4 & c5))
+        else if ((c1 & c2 & c3 & c4) | (c1 & c2 & c3 & c5) | (c1 & c2 & c4 & c5) | (c1 & c3 & c4 & c5) | (c2 & c3 & c4 & c5))
             $display ("1110000");
         else if ((c1 & c2 & c3 & ~c4 & ~c5) | (c1 & c2 & ~c3 & c4 & ~c5) | (c1 & c2 & ~c3 & ~c4 & c5)| (c1 & ~c2 & c3 & c4 & ~c5) | (c1 & ~c2 & c3 & ~c4 & c5) | (c1 & ~c2 & ~c3 & c4 & c5) | (~c1 & c2 & c3 & c4 & ~c5) | (~c1 & c2 & c3 & ~c4 & c5) | (~c1 & c2 & ~c3 & c4 & c5) | (~c1 & ~c2 & c3 & c4 & c5))
             $display ("1101101");
@@ -23,7 +24,7 @@ module dispcomp;
 		else if (~c1 & ~c2 & ~c3 & ~c4 & ~c5)
 			//o caso em que nenhum dos computadores está sendo usado vem antes do em que apenas 1 é usado pois isso diminui a quantidade de termos abaixo
             $display ("1011011");
-        else if ((~c1 & ~c2 & ~c3 & ~c4) | (~c1 & ~c2 & ~c3 & ~c5) | (~c1 & ~c2 & ~c4 & ~c5) | (~c1 & ~c2 & ~c3 & ~c4) | (~c2 & ~c3 & ~c4 & ~c5))
+        else if ((~c1 & ~c2 & ~c3 & ~c4) | (~c1 & ~c2 & ~c3 & ~c5) | (~c1 & ~c2 & ~c4 & ~c5) | (~c1 & ~c3 & ~c4 & ~c5) | (~c2 & ~c3 & ~c4 & ~c5))
             $display ("0110011");
         $finish;
     end
