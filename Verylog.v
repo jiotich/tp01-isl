5d788f18baba98e0033bbfdf1b2609e9fdf36c59
João Francisco Olivetti <jiotich@gmail.com>
2021-03-20 22:45:10 -0300

    Corrected number 1 to the normal representation
    I previously thought 1110000 corresponded to one, but turns out the right display is 0110000. Fixed that.

--- Verylog.v
+++ Verylog.v
@@ -2,7 +2,7 @@ module verylog(comps, segs);
 	input wire [4:0] comps;
 	output wire [6:0] segs;
 	
-	assign segs[6] = ~((~comps[0] & ~comps[1] & ~comps[2] & ~comps[3]) | (~comps[0] & ~comps[1] & ~comps[2] & ~comps[4]) | (~comps[0] & ~comps[1] & ~comps[3] & ~comps[4]) | (~comps[0] & ~comps[2] & ~comps[3] & ~comps[4]) | (~comps[1] & ~comps[2] & ~comps[3] & ~comps[4])) | (~comps[0] &~comps[1] & ~comps[2] & ~comps[3] & ~comps[4]);
+	assign segs[6] = ~((~comps[0] & ~comps[1] & ~comps[2] & ~comps[3] & comps[4]) | (~comps[0] & ~comps[1] & ~comps[2] & comps[3] & ~comps[4]) | (~comps[0] & ~comps[1] & comps[2] & ~comps[3] & ~comps[4]) | (~comps[0] & comps[1] & ~comps[2] & ~comps[3] & ~comps[4]) | (comps[0] & ~comps[1] & ~comps[2] & ~comps[3] & ~comps[4])) & ~((~comps[0] & comps[1] & comps[2] & comps[3] & comps[4]) | (comps[0] & ~comps[1] & comps[2] & comps[3] & comps[4]) | (comps[0] & comps[1] & ~comps[2] & comps[3] & comps[4]) | (comps[0] & comps[1] & comps[2] & ~comps[3] & comps[4]) | (comps[0] & comps[1] & comps[2] & comps[3] & ~comps[4]));
 	
 	assign segs[5] = ~(~comps[0] & ~comps[1] & ~comps[2] & ~comps[3] & ~comps[4]);
 	
